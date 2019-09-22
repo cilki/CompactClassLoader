a55cefe0d2119f3e8f5288f64a82f93b0b749715
Tyler Cook <tcc@sandpolis.com>
2019-09-22 14:24:41 -0500

    WIP

--- src/main/java/com/github/cilki/compact/CompactClassLoader.java
+++ src/main/java/com/github/cilki/compact/CompactClassLoader.java
@@ -24,6 +24,7 @@ import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -31,10 +32,10 @@ import java.util.stream.Stream;
 
 /**
  * A {@link ClassLoader} that can load nested jar files and bootstrap
- * applications.<br>
- * <br>
- * Note: Although this class is not entirely thread safe, it can safely load
- * classes in parallel.
+ * applications.
+ * 
+ * <p>
+ * This classloader uses the parent-first delegation model.
  * 
  * @author cilki
  */
@@ -49,15 +50,24 @@ public final class CompactClassLoader extends ClassLoader {
 
 	static {
 		log.setLevel(Level.parse(System.getProperty("ccl.level", "warning").toUpperCase()));
+		var handler = new ConsoleHandler();
+		handler.setLevel(Level.FINE);
+		log.addHandler(handler);
 
 		registerAsParallelCapable();
+	}
+
+	private static CompactClassLoader system;
+
+	public static CompactClassLoader getSystem() {
+		return system;
 	}
 
 	/**
 	 * A list of classloaders responsible for loading {@link URL}s introduced by
 	 * {@link #add(URL)}.
 	 */
-	private final List<NodeClassLoader> components;
+	protected final List<ClassLoader> components;
 
 	/**
 	 * Build an empty {@link CompactClassLoader}.
@@ -67,18 +77,15 @@ public final class CompactClassLoader extends ClassLoader {
 	public CompactClassLoader(ClassLoader parent) {
 		super(parent);
 		components = new LinkedList<>();
-	}
 
-	/**
-	 * Build a {@link CompactClassLoader} for the current jar file.
-	 * 
-	 * @param recursive Whether all encountered jars will also be added
-	 * @throws IOException If an I/O exception occurs while reading the current jar
-	 */
-	public CompactClassLoader(boolean recursive) throws IOException {
-		this(ClassLoader.getSystemClassLoader());
-
-		add(getClass().getProtectionDomain().getCodeSource().getLocation(), recursive);
+		// Examine the stack to determine whether this classloader is being built by the
+		// runtime
+		var stack = Thread.currentThread().getStackTrace();
+		if (stack[stack.length - 1].getMethodName().equals("initPhase3")) {
+			if (system != null)
+				throw new RuntimeException();
+			system = this;
+		}
 	}
 
 	/**
@@ -102,32 +109,57 @@ public final class CompactClassLoader extends ClassLoader {
 	 */
 	public void add(URL url, boolean recursive) throws IOException {
 		Objects.requireNonNull(url);
-		if (components.stream().anyMatch(c -> c.findBaseUrl(url)))
-			throw new IllegalArgumentException("The URL is already has a classloader in the hierarchy");
+		if (components.stream().filter(NodeClassLoader.class::isInstance).map(NodeClassLoader.class::cast)
+				.anyMatch(c -> c.findBaseUrl(url)))
+			throw new IllegalArgumentException("The URL already has a classloader in the hierarchy");
 
-		components.add(new NodeClassLoader(getParent(), URLFactory.toNested(url), recursive));
+		components.add(new NodeClassLoader(this, URLFactory.toNested(url), recursive));
+	}
+
+	public void add(CompactClassLoader cl) {
+		Objects.requireNonNull(cl);
+		if (components.contains(cl))
+			throw new IllegalArgumentException("The classloader already exists in the hierarchy");
+
+		components.add(cl);
 	}
 
 	@Override
 	protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+		return loadClass(name, null);
+	}
 
-		// Shortcut for standard classes
-		if (name.startsWith("java."))
-			return getSystemClassLoader().loadClass(name);
+	Class<?> loadClass(String name, ClassLoader skip) throws ClassNotFoundException {
+		log.finer(() -> "Loading class: " + name);
+
+		// Delegate to parent first
+		try {
+			return super.loadClass(name, false);
+		} catch (ClassNotFoundException e) {
+			// continue
+		}
 
 		// Try components
-		for (NodeClassLoader component : components) {
-			try {
-				return component.loadClass(name);
-			} catch (ClassNotFoundException e) {
-				// Rethrow if not empty
-				if (e.getCause() != null)
-					throw e;
+		return loadDown(name, skip);
+	}
+
+	Class<?> loadDown(String name, ClassLoader skip) throws ClassNotFoundException {
+		for (ClassLoader component : components) {
+			if (component != skip) {
+				try {
+					if (component instanceof NodeClassLoader)
+						return ((NodeClassLoader) component).loadDown(name, null);
+					if (component instanceof CompactClassLoader)
+						return ((CompactClassLoader) component).loadDown(name, null);
+				} catch (ClassNotFoundException e) {
+					// Rethrow if not empty
+					if (e.getCause() != null)
+						throw e;
+				}
 			}
 		}
 
-		// Delegate to parent
-		return super.loadClass(name, resolve);
+		throw new ClassNotFoundException();
 	}
 
 	@Override
@@ -143,7 +175,27 @@ public final class CompactClassLoader extends ClassLoader {
 
 	@Override
 	public Stream<URL> resources(String name) {
-		return components.stream().flatMap(component -> component.resources(name));
+		return resources(name, null);
+	}
+
+	Stream<URL> resources(String name, ClassLoader skip) {
+		log.finer(() -> "Loading resources: " + name);
+
+		if (getParent() != null)
+			return Stream.concat(getParent().resources(name), resourcesDown(name, skip));
+		else
+			return resourcesDown(name, skip);
+	}
+
+	Stream<URL> resourcesDown(String name, ClassLoader skip) {
+		return components.stream().filter(component -> component != skip).flatMap(component -> {
+			if (component instanceof NodeClassLoader)
+				return ((NodeClassLoader) component).resourcesDown(name, null);
+			if (component instanceof CompactClassLoader)
+				return ((CompactClassLoader) component).resourcesDown(name, null);
+
+			throw new RuntimeException();
+		});
 	}
 
 	@Override
@@ -155,4 +207,5 @@ public final class CompactClassLoader extends ClassLoader {
 	public Enumeration<URL> getResources(String name) throws IOException {
 		return findResources(name);
 	}
+
 }
